4de148323dffe99bd8fe3bfbffc76aeff3569752
tbnsok40 <tbnsok40@naver.com>
2020-12-03 20:53:09 +0900

    week4

---
+++ src/java_basic/week4/homework01.java
@@ -0,0 +1,42 @@
+package java_basic.week4;
+import org.kohsuke.github.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+// 출력되는데 시간이 걸린다.
+// 각 주차를 모두 돌기 때문에 ISSUE_COUNT는 18
+//
+public class homework01 {
+    private static final int ISSUE_COUNT = 18;
+
+    public static void main(String[] args) throws IOException {
+        GitHub github = new GitHubBuilder().withOAuthToken("be0173f1b1d69ba49d525ae42fd6603a15967eb9").build();
+        GHRepository repository = github.getRepository("tbnsok40/live-study").getSource();
+        GHIssue issue;
+        List<GHIssueComment> comments;
+
+        // hm은 for문 밖에서 선언 => 초기화 되면 안되는 객체
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (int i = 1; i <= ISSUE_COUNT; ++i) {
+            issue = repository.getIssue(i);
+            comments = issue.getComments();
+            HashSet<String> hs = new HashSet<>();
+            String name = "";
+            int size = comments.size();
+            for(int j = 0; j < size; ++j) {
+                try {
+                    name = comments.get(j).getUser().getName();
+                    if(name != null && !hs.contains(name)) {
+                        hm.put(name, hm.getOrDefault(name, 0) + 1); // +1 은 각 주차마다 달은 댓글
+                    };
+                } catch (IOException e) {
+                    System.out.println("작업 도중 오류가 발생 하였습니다. [" + i + " 주차, 이름 = " + name + "]");
+                }
+            }
+        }
+        System.out.println("========== 참여율 ==========");
+        hm.forEach((s, i) -> System.out.printf("%-20s -> %.2f%%\n", s, (i / (float)ISSUE_COUNT * 100)));
+    }
+}
